0958fecf4599123fad881465f9a8a93d5d3e76be
Asukirina <chenlinbo.mail.ustc.edu.cn>
2025-05-07 09:55:57 +0800

    添加了nano内核的C代码，初步改写irq.c

--- src/RusT-thread/src/main.rs
+++ src/RusT-thread/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+
+
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
@@ -11,7 +13,7 @@ use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 mod rtdef;
-
+mod irq;
 #[entry]
 fn main() -> ! {
     // asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
