c48343c641052b227b2b5226cb30fa5965dae754
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2025-06-26 15:52:32 +0800

    添加一些logo

--- src/RusT-thread/src/test/mod.rs
+++ src/RusT-thread/src/test/mod.rs
@@ -15,29 +15,23 @@ pub mod test_scheduler;
 pub mod test_interupt;
 pub mod example;
 pub mod test_cell;
+pub mod test_all;
 
 
-#[cfg(feature = "test_timer")]
-pub mod test_timer;
-#[cfg(feature = "test_small_mem")]
-pub mod test_small_mem;
-#[cfg(feature = "test_allocator_compare")]
-pub mod test_allocator_compare;
+
+// pub mod test_timer;
+
+// pub mod test_small_mem;
+
+
 
 
 pub fn run_all_tests() {
-    #[cfg(feature = "test_small_mem")]
-    {
-        hprintln!("开始小内存管理测试...");
-        test::test_small_mem::run_simple_mem_tests();
-        hprintln!("小内存管理测试完成！");
-    }
-    #[cfg(feature = "test_timer")]
-    {
-        hprintln!("开始定时器测试...");
-        test_timer::run_all_timer_tests();
-        hprintln!("定时器测试完成！");
-    }
+    test_all::print_logo();
+
+    // test::test_small_mem::run_simple_mem_tests();
+    // test_timer::run_all_timer_tests();
+    
     // test_mem::test_vec();
     // test_mem::test_alloc_dealloc();
     // test_mem::test_box();
